9f5f35d372d6dcddc967ee83e452d584796408a4
redstorm-hub <nigussieabreham80@gmail.com>
2022-08-18 16:41:21 -0700

    Added container components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import React from "react"
+import { Container } from "../Components/index"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const indexPage = () => {
+  return (
+    <Container>
+      <h1>Hello world!</h1>
+    </Container>
+  )
 }
 
+export default indexPage
