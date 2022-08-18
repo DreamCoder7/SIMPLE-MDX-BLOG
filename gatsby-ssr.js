2f5a4166f22abc4b996222dcea6acac5635d2127
redstorm-hub <nigussieabreham80@gmail.com>
2022-08-17 19:00:28 -0700

    Added global style

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,28 @@
 import React from "react"
-import { ThemeProvider } from "styled-components"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 import Theme from "./src/themes/themes"
 
-export default wrapRootElement = ({ element }) => (
-  <ThemeProvider theme={Theme}>{element}</ThemeProvider>
-)
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    html, body {
+        font-family: ${props => props.theme.fonts.main};
+        height: 100%;
+        background-color: ${props => props.theme.colors.light1};
+    }
+`
+
+export const wrapRootElement = ({ element }) => {
+  console.log(element)
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyles />
+      {element}
+    </ThemeProvider>
+  )
+}
